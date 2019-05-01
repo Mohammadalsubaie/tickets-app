9c46f1f01312b066d70b59b9fff260013f5cbad6
Mohammed <mohammedalsubaie14@gmail.com>
2019-05-01 11:11:51 -0700

    edited index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hi there!</div>;
+    return (
+        <div className="ui container comments">
+            <div className= "comment">
+                <a href="/" className="avatar">
+                    img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href='/' className="author">
+                      sam
+                    </a>
+                    <div className="matedata">
+                      <span calssName="data">today at 6:00PM</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
     
 };
 
