7433189cee0659234a7cc246a345365d5f58dd04
Ibrahim Al-Khalifah <ibraheam@Ibrahims-MacBook-Pro-3.local>
2019-05-01 21:38:58 +0300

    changed function component to class component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return (
-        <div className="ui container comments">
-            <div className= "comment">
-                <a href="/" className="avatar">
-                    img alt="avatar" />
-                </a>
-                <div className="content">
-                    <a href='/' className="author">
-                      sam
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="ui container comments">
+                <div className="comment">
+                    <a href="/" className="avatar">
+                        <img alt="avatar" />
                     </a>
-                    <div className="matedata">
-                      <span calssName="data">today at 6:00PM</span>
+                    <div className="content">
+                        <a href='/' className="author">
+                            sam
+                        </a>
+                        <div className="matedata">
+                            <span calssName="data">today at 6:00PM</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
-    
-};
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
