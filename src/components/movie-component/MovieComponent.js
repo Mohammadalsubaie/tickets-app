5ef07c1b3d72d878237ce09f0b33627153fd6fe4
Ibrahim Al-Khalifah <ibraheam@Ibrahims-MacBook-Pro-3.local>
2019-05-14 12:36:00 +0300

    edit

--- src/components/movie-component/MovieComponent.js
+++ src/components/movie-component/MovieComponent.js
@@ -1,33 +1,23 @@
 
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText, Container, Col } from 'reactstrap';
 
 
 export default class MovieComponent extends Component {
     render() {
         return (
-            <div className="ui container comments">
-                <div className="comment">
-                    <a href="/" className="avatar">
-                        <img alt="avatar" />
-                    </a>
-                    <div className="content">
-                        <a href='/' className="author">
-                            sam
-                        </a>
-                        <div className="matedata">
-                            <span calssName="data">today at 6:00PM</span>
-                        </div>
-                    </div>
-                </div>
-                <Form>
-                    <FormGroup>
-                        <Label for="exampleEmail">Email</Label>
-                        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-                    </FormGroup>
-                </Form>
-                <input type="submit" value="Submit" />
-            </div>
+            <Container>
+
+                <Col sm="4" xs="4" md="4">
+                    <Form>
+                        <FormGroup>
+                            <Label for="exampleEmail">Email</Label>
+                            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                        </FormGroup>
+                    </Form>
+                </Col>
+
+            </Container>
         )
     }
 }
